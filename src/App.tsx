a5f4e4b9dabe204428f2aec7f54777bc47bc4781
Lukas <lukasdaugela197@gmail.com>
2023-02-20 20:31:55 +0200

    Added Button component and style for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="app">
       <h1 className="app__title">Reiz Tech Assignment</h1>
+      <Button text="area < Lithuania" />
     </div>
   );
 }
