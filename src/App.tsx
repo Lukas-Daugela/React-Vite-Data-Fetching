e296f1f5572d65a2e4647d02045ae6c60f4be4d1
Lukas <lukasdaugela197@gmail.com>
2023-02-21 11:44:46 +0200

    Added page content and some body styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
 import "./App.scss";
+import SectionCountries from "./components/SectionCountries/SectionCountries";
+import PageHeader from "./components/Header/PageHeader";
+import Button from "./components/Button/Button";
+import upAndDownArrow from "/assets/upAndDownArrows.svg";
 
 function App() {
+  const testArray: {
+    country: string;
+    region: string;
+    areaSize: number;
+  }[] = [
+    {
+      country: "Lithuania",
+      region: "Europe",
+      areaSize: 325151516,
+    },
+    {
+      country: "Latvia",
+      region: "Europe",
+      areaSize: 187891516,
+    },
+    {
+      country: "New York",
+      region: "United States",
+      areaSize: 98989962135,
+    },
+  ];
+
   return (
-    <div className="app">
-      <h1 className="app__title">Reiz Tech Assignment</h1>
-    </div>
+    <>
+      <PageHeader title="Reiz Tech Assignment" />
+      <main className="main">
+        <div className="main__buttons-container">
+          <div className="buttons-container__filter-buttons">
+            <Button text="area < Lithuania" />
+            <Button text="Oceania region" />
+          </div>
+          <Button text="A - Z">
+            <img src={upAndDownArrow} alt="" />
+          </Button>
+        </div>
+        <SectionCountries formatedData={testArray} />
+      </main>
+    </>
   );
 }
 
