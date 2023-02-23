cc5715a2cadc8e99b0aa9697356ce5590f50d475
Lukas <lukasdaugela197@gmail.com>
2023-02-23 11:21:11 +0200

    Moved main logic and elements to a new component

---
+++ src/components/SectionMain/SectionMain.tsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from "react";
+
+import {
+  fetchApiData,
+  filterCountriesLessThan,
+  filterCountriesRegion,
+  findArea,
+  reverseData,
+} from "../../services/fetchAndFilter";
+import Button from "../Button/Button";
+import PagePagination from "../PagePagination/PagePagination";
+import SectionCountries from "../SectionCountries/SectionCountries";
+import upAndDownArrow from "/assets/upAndDownArrows.svg";
+import "./SectionMain.scss";
+
+function SectionMain() {
+  const [fetchedCountries, setFetchedCountries] = useState<any[]>([]);
+  const [currentCountries, setCurrentCountries] = useState<any[]>([]);
+  const [descendingOrder, setDescendingOrder] = useState<boolean>();
+  const [isAreaFilter, setIsAreaFilter] = useState<boolean>(false);
+  const [isRegionFilter, setIsRegionFilter] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const countriesPerPage: number = 5;
+
+  const amountOfPages = Math.ceil(currentCountries.length / countriesPerPage);
+  const isLastPage = currentPage === amountOfPages;
+
+  const fetchData = async () => {
+    await fetchApiData().then((data) => {
+      setCurrentCountries(data);
+      setFetchedCountries(data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (descendingOrder) {
+      handleReverse(currentCountries);
+    } else if (!descendingOrder) {
+      handleReverse(currentCountries);
+    }
+  }, [descendingOrder]);
+
+  const handleReverse = (dataToReverse: any[]) => {
+    const reversedData = reverseData([...dataToReverse]);
+    setCurrentCountries(reversedData);
+  };
+
+  const handleLessThanArea = async () => {
+    if (!isAreaFilter) {
+      const areaSize = await findArea(fetchedCountries, "Lithuania");
+      const filteredCountries = filterCountriesLessThan(
+        fetchedCountries,
+        areaSize
+      );
+      setCurrentCountries(filteredCountries);
+      setIsAreaFilter(true);
+      setIsRegionFilter(false);
+      setCurrentPage(1);
+    } else return;
+  };
+
+  const handleFilterRegion = () => {
+    if (!isRegionFilter) {
+      const filteredCountries = filterCountriesRegion(
+        fetchedCountries,
+        "Oceania"
+      );
+      setCurrentCountries(filteredCountries);
+      setIsRegionFilter(true);
+      setIsAreaFilter(false);
+      setCurrentPage(1);
+    } else return;
+  };
+
+  const handleAllCuntries = () => {
+    setCurrentCountries(fetchedCountries);
+    setIsRegionFilter(false);
+    setIsAreaFilter(false);
+    setCurrentPage(1);
+  };
+
+  // Slicing current data to display required amount of elements in on page
+  const indexOfLastCountry = currentPage * countriesPerPage;
+  const indexOfFirstCountry = indexOfLastCountry - countriesPerPage;
+  const displayedCountries = currentCountries.slice(
+    indexOfFirstCountry,
+    indexOfLastCountry
+  );
+
+  return (
+    <>
+      <main className="main">
+        <div className="main__buttons-container">
+          <div className="buttons-container__filter-buttons">
+            <Button
+              type={"filter"}
+              func={handleAllCuntries}
+              text="All countries"
+            />
+            <Button
+              type={"filter"}
+              func={handleLessThanArea}
+              text="Area < Lithuania"
+            />
+            <Button
+              type={"filter"}
+              func={handleFilterRegion}
+              text="Oceania region"
+            />
+          </div>
+          <Button
+            type={"sort"}
+            currentState={descendingOrder}
+            func={setDescendingOrder}
+            text="A - Z"
+          >
+            <img src={upAndDownArrow} alt="" />
+          </Button>
+        </div>
+        {currentCountries && (
+          <SectionCountries
+            isLastPage={isLastPage}
+            formatedData={displayedCountries}
+          />
+        )}
+      </main>
+      <PagePagination
+        amountOfPages={amountOfPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
+  );
+}
+
+export default SectionMain;
